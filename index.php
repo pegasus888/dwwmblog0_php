f4a5b172709ed7095130b1b2937fbc6c79aa98df
pegasus888 <85719175+pegasus888@users.noreply.github.com>
2024-10-12 22:11:15 +0200

    Env setup

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo 'Home page';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DWWM Blog</title>
+    <link rel="stylesheet" href="/css/custom.css">
+</head>
+<body>
+<h1 class="text-altmyflamboyant lead">Index page</h1>
+</body>
+</html>
